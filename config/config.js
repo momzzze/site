c7b271ecff0b6ef118d7afc5989bf5d7a6b61536
momzzze <momzzze@abv.bg>
2016-11-26 22:50:18 +0200

    s
    s

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    development: {
+        rootFolder: path.normalize(path.join(__dirname, '/../')),
+        connectionString: 'mongodb://localhost:27017/blog'
+    },
+    production:{}
+};
+
+
+
+
